fa6ae9b9f563211d05bb79af4a6db6450a20edb0
Sajjad Hossain <sajjad.hira12@gmail.com>
2016-05-29 16:35:49 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,14 +1,6 @@
 <?php
 function clean($string) {
-   $search=array("`","~","!","@","#","$",'%',"^","&","*","(",")","_","+","=",",",".","?","/","<",">",":",";","'",'"'); 
-   $replace=array("","","","","","","","","","","","","","","","","","","","","","","","",""); 
-   $string = str_replace($search,$replace,$string); 
-   $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
-   $string = str_replace('--', '-', $string); // Replaces all spaces with hyphens.
-   $string = str_replace('--', '-', $string); // Replaces all spaces with hyphens.
-   $string = trim($string);
-   $string = rtrim($string,'-');
-   $string = strtolower($string);
+   $string = strtolower( preg_replace('@[\W_]+@', '-', $string) );
    return $string; // Removes special chars.
 }
 
